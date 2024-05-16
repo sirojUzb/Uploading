19a927b8790b5a7b0d3e902d6fd0342740933243
sirojUzb <sirojiddinmamatovuzb@gmail.com>
2024-05-17 05:04:49 +0900

    fifth

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
-import { Button, Card, Modal } from "antd";
+import { Button, Card, Form, Input, InputNumber, Modal, Upload } from "antd";
 // import { PlusOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
 function App() {
   const [flowers, setFlowers] = useState([]);
   const [open, setOpen] = useState(false);
-  // const onSubmit = (values) => {
-  //   console.log(values);
-  // };
+  const onFinish = (values) => {
+    console.log(values);
+    const shouldUpload = {
+      title: values.title,
+      price: values.price,
+      main_image: values.main_image.file.response.image_url.url,
+    };
+    setOpen(false);
+  };
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
@@ -16,22 +22,57 @@ function App() {
       );
       const data = await response.json();
       setFlowers(data.data);
-      console.log(data);
     };
     fetchData();
   }, []);
   return (
     <div className="flex justify-center items-center flex-col">
       <Modal
-        okText="create"
+        okText="Create"
         onOk={() => {
           setOpen(false);
         }}
         open={open}
         onCancel={() => setOpen(false)}
         title="Add flower"
+        footer={false}
       >
-        123
+        <Form onFinish={onFinish}>
+          <Form.Item
+            label="Title"
+            name="title"
+            rules={[{ required: true, message: "Please input your title" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Upload"
+            name="main_image"
+            rules={[{ required: true, message: "Please upload your image" }]}
+          >
+            <Upload
+              action={
+                "http://localhost:8080/api/upload?access_token=64bebc1e2c6d3f056a8c85b7"
+              }
+              name="image"
+            >
+              <Button>Upload</Button>
+            </Upload>
+          </Form.Item>
+          <Form.Item
+            label="Price"
+            name="price"
+            rules={[{ required: true, message: "Please input your price" }]}
+          >
+            <InputNumber />
+          </Form.Item>
+          <Form.Item>
+            <Button danger onClick={() => setOpen(false)}>
+              Cancel
+            </Button>
+            <Button htmlType="submit">Create</Button>
+          </Form.Item>
+        </Form>
       </Modal>
       <div className="absolute top-2 right-2">
         <Button onClick={() => setOpen(true)}>Add</Button>
@@ -55,6 +96,6 @@ function App() {
 }
 
 export default App;
-// 31-daqiqadan koraver
+// 2-videodan koraver
 // bugun tugatish kk
 // terminalda cd "greenshop-b" deb ochsa boladi
